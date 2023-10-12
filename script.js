e75c41a23da93e80ccb1887e03726de15c57e4d8
Dineshjnld <dineshjnld22@gmail.com>
2023-10-12 14:32:21 +0530

    update

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const menuButton = document.querySelector(".logo");
-const menuList = document.querySelector(".menu");
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.getElementById("login-form");
+    const loginButton = document.getElementById("login-button");
 
-menuButton.addEventListener("click", () => {
-    menuList.classList.toggle("active");
+    loginForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+        if (username === "code" && password === "create") {
+            window.location.href = "explore.html";
+        } else {
+            alert("Incorrect username or password. Please try again.");
+        }
+    });
 });
